bcc22b199ecdc8de902b85d144ea1f2ea46f0dd6
mikekutilek <mikekutilek>
2018-06-11 22:27:34 -0400

    adding situational hitting methods

--- matchup_tool/matchup_tool.py
+++ matchup_tool/matchup_tool.py
@@ -1,24 +1,12 @@
 import pandas
 import numpy
+import math
 
 pitch_value_batting_data = pandas.read_csv('c:/Users/makut/Documents/Data/Fangraphs/Batting/2018/Pitch Value Batting Data.csv')
 pitch_type_pitching_data = pandas.read_csv('c:/Users/makut/Documents/Data/Fangraphs/Pitching/2018/Pitch Type Pitching Data.csv')
-
-
-def calc_matchup(team, pitcher):
-	batter_data = pitch_value_batting_data.loc[pitch_value_batting_data['Team'] == team]
-	batter_data = batter_data.fillna(0)
-
-	for index, batter in batter_data.iterrows():
-		#print(batter['Name'])
-		calc_pitch_score(batter['Name'], pitcher)
-
-	#calc_pitch_score('Starling Marte', 'Patrick Corbin')
-	#calc_pitch_score('Gregory Polanco', 'Patrick Corbin')
-	#calc_pitch_score('Francisco Cervelli', 'Patrick Corbin')
-	#calc_pitch_score('Corey Dickerson', 'Patrick Corbin')
-	#calc_pitch_score('Josh Bell', 'Patrick Corbin')
-
+runners_in_scoring_batting_data = pandas.read_csv('c:/Users/makut/Documents/Data/Fangraphs/Batting/2018/Runners in Scoring Batting Data.csv')
+runners_on_batting_data = pandas.read_csv('c:/Users/makut/Documents/Data/Fangraphs/Batting/2018/Runners On Batting Data.csv')
+bases_empty_batting_data = pandas.read_csv('c:/Users/makut/Documents/Data/Fangraphs/Batting/2018/Bases Empty Batting Data.csv')
 
 def clean_data(data):
 	replace_percents(data)
@@ -30,6 +18,15 @@ def replace_percents(data):
 
 def convert_to_float(obj):
 	return obj.astype('float64')
+
+def calc_matchup(team, pitcher):
+	batter_data = pitch_value_batting_data.loc[pitch_value_batting_data['Team'] == team]
+	batter_data = batter_data.fillna(0)
+	#batter_data = pitch_value_batting_data
+	print("\n" + team + " vs. " + pitcher)
+	for index, batter in batter_data.iterrows():
+		#print(batter['Name'])
+		calc_pitch_score(batter['Name'], pitcher)
 
 def calc_pitch_score(batter, pitcher):
 	#total_pitch_score = 0
@@ -63,6 +60,7 @@ def calc_pitch_score(batter, pitcher):
 
 	pitch_score = (wFB[batter_index] * fb_percent[pitcher_index]) + (wSL[batter_index] * sl_percent[pitcher_index]) + (wCT[batter_index] * ct_percent[pitcher_index]) + (wCB[batter_index] * cb_percent[pitcher_index]) + (wCH[batter_index] * ch_percent[pitcher_index]) + (wSF[batter_index] * sf_percent[pitcher_index])
 	pitch_score = pitch_score / 100
+
 	#total_pitch_score += pitch_score
 	print('Pitch score for: ' + str(batter) + ' vs. ' + str(pitcher) + ': ' + str(round(pitch_score, 2)))
 
@@ -109,7 +107,36 @@ def calc_team_pitch_value(team):
 	w_data['W/C'] = wc
 	print(w_data.sort_values(by='W', ascending=False))
 
+def calc_situational_hitting_diff():
+	batter_data = runners_on_batting_data.loc[runners_on_batting_data['PA'] > 25]
+	wRC1 = batter_data['wRC+']
+	batter_data = bases_empty_batting_data.loc[bases_empty_batting_data['PA'] > 25]
+	wRC2 = batter_data['wRC+']
+	
+	wRC1_data = pandas.DataFrame()
+	wRC1_data['Name'] = runners_on_batting_data['Name']
+	wRC1_data['wRC+ (Runners On)'] = round(wRC1).astype("int")
+	wRC2_data = pandas.DataFrame()
+	wRC2_data['Name'] = bases_empty_batting_data['Name']
+	wRC2_data['wRC+ (Bases Empty)'] = round(wRC2).astype("int")
+
+	diff_data = wRC1_data.merge(wRC2_data, on='Name', how='left').fillna(0)
+	diff_data['wRC+ (Bases Empty)'] = diff_data['wRC+ (Bases Empty)'].astype("int")
+	diff_data['Difference'] = round(diff_data['wRC+ (Runners On)'] - diff_data['wRC+ (Bases Empty)']).astype("int")
+	print(diff_data.sort_values(by='Difference', ascending=False))
+	diff_data.to_csv('c:/Users/makut/Documents/Data/Fangraphs/Batting/2018/Situational Difference Batting Data.csv')
+
+def player_calc_situational_hitting_diff(batter):
+	batter_data = runners_on_batting_data.loc[runners_on_batting_data['Name'] == batter]
+	wRC1 = batter_data['wRC+']
+	batter_data = bases_empty_batting_data.loc[bases_empty_batting_data['Name'] == batter]
+	wRC2 = batter_data['wRC+']
+	print(round(float(wRC1) - float(wRC2), 2))
 
 #replace_percents(pitch_type_pitching_data)
-calc_matchup('Pirates', 'Patrick Corbin')
+#calc_matchup('Pirates', 'Patrick Corbin')
+#calc_matchup('Diamondbacks', 'Joe Musgrove')
+#calc_matchup('Red Sox', 'Dylan Bundy')
+#calc_matchup('Orioles', 'Steven Wright')
 #calc_team_pitch_value("Pirates")
+calc_situational_hitting_diff()
